8aa55238a94612efcb933e597d39d4ae520dd679
Kaique Marlon <kaiquefraday@gmail.com>
2025-02-01 21:14:01 -0300

    commit

--- Dev Samurai/meu primeiro código/script.js
+++ Dev Samurai/meu primeiro código/script.js
@@ -1,1 +1,4 @@
-alert("olá, mundo");
+var nome = prompt("Digite o seu nome");
+var idade = prompt("Digite a sua idade");
+var cidade = prompt("Digite o nome da sua cidade");
+alert("Meu nome é" + nome + " ,tenho" + idade + " anos e moro em" + cidade);
---
+++ Dev Samurai/operações/script.js
@@ -0,0 +1,6 @@
+var num1 = Number(prompt("digite o primeiro número"));
+var num2 = Number(prompt("Digite o segundo número"));
+alert("soma:" + (num1 + num2));
+alert("subtração:" + (num1 - num2));
+alert("multiplicação:" + num1 * num2);
+alert("divisão:" + num1 / num2);
